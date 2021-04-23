b88679acbb76213cc78a129ead41ef6eaf3d4b56
Nicholas Anderson <nkanderson00@gmail.com>
2021-04-23 13:06:31 -0400

    Update server.py

--- server.py
+++ server.py
@@ -7,48 +7,6 @@ import threading
 PACKET_SIZE = 256
 PORT = 42069
 PROTOCOL = "UDP" #can be UDP or TCP
-PORT_DESCRIPTION = "my python server"
-
-
-class Commands:
-
-	def __init__(self, s):
-		self.s = s
-		self.help_message = "Type \"help\" for a list of commands."
-		self.commands = {"help":self.help, "quit":self.quit, "ip":self.ip, "port":self.port,
-						 "protocol":self.protocol}
-		
-	def is_command(self, command):
-		return command in self.commands
-		
-	def run(self, command):
-		split = command.split()
-
-		if split and self.is_command(split[0].lower()):
-			args = " ".join(split[1:])
-			self.commands[split[0].lower()](args)
-		else:
-			print("Command not found. "+self.help_message)
-	
-	def help(self, command):
-		"""Displays help message"""
-		print("Available commands:\n\n"+"\n".join(f"{k}: {v.__doc__}" for k, v in self.commands.items())+"\n")
-	
-	def quit(self, args):
-		"""Stop the server and exit"""
-		self.s.stop_flag = True
-		
-	def ip(self, args):
-		"""See the external IP address of the server"""
-		print(self.s.external_ip)
-		
-	def port(self, args):
-		"""See the port of the server"""
-		print(self.s.port)
-		
-	def protocol(self, args):
-		"""See the connection protocol of the server"""
-		print(self.s.protocol)
 
 	
 class Server:
@@ -58,13 +16,7 @@ class Server:
 		self.port = port
 		self.protocol = protocol
 		self.packet_size = packet_size
-		self.description = description
 		self.timeout = timeout
-		self.stop_flag = False
-	
-		self.get_device_info()
-		self.forward()
-	
 		self.history = []
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		self.sock.bind((self.local_ip, self.port))
@@ -72,22 +24,9 @@ class Server:
 		self.clients = {}
 		
 		
-	def get_device_info(self):
-		output = subprocess.check_output(["upnpc", "-s"]).decode("utf-8").split("\r\n")
-		self.desc_url = [i.split()[-1] for i in output if "desc" in i.lower()][0]
-		self.addresses = [i.split()[-1] for i in output if "address" in i.lower()]
-		self.local_ip = self.addresses[0]
-		self.external_ip = self.addresses[1]
-		
-
-	def forward(self):
-		output = subprocess.call(["upnpc", "-u", self.desc_url, "-e", self.description,
-		"-a", self.local_ip, str(self.port), str(self.port), self.protocol], stderr=subprocess.DEVNULL)
-		
-		
 	def run_daemon(self):
 	
-		while not self.stop_flag:
+		while True:
 		
 			now = time.time()
 			now_ms = int(now*1000)
@@ -110,9 +49,8 @@ class Server:
 							del self.clients[address]
 							print(now_ms, "DISCONNECTED:", address)
 
-						
 				if not self.clients:
-					time.sleep(0.05)
+					time.sleep(0.01)
 				
 		print("Server stopped")
 		
@@ -121,20 +59,9 @@ class Server:
 		
 		threading.Thread(target=self.run_daemon, daemon=True).start()
 		print(f"Server is running on port: {self.port} and external ip: {self.external_ip}")
-		
-		commands = Commands(self)
-		print(commands.help_message)
-		
-		while not self.stop_flag:
-			command = input(">> ")
-			commands.run(command)
-				
 
-
-
-
-				
+	
 if __name__ == "__main__":
-	s = Server(PORT, PROTOCOL, PACKET_SIZE, PORT_DESCRIPTION)
+	s = Server(PORT, PROTOCOL, PACKET_SIZE)
 	s.run()
 	
